8d9dc218e96098831b514374d38a49ac8051c99f
valens niyonsenga <95166477+valens200@users.noreply.github.com>
2024-10-30 01:06:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+server: {
+    host: '10.10.82.249',  // Explicitly bind to this IP
+    port: 5173
+  },
   plugins: [react()],
   resolve: {
     alias: {
