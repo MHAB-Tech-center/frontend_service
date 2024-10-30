27e1a68b48649a8c9ec879f3d5a29a264e4c4df9
valens200 <valensniyonsenga2003@gmail.com>
2024-10-30 16:25:34 +0200

    added config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
+  preview: {
+    port: 5173,
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
