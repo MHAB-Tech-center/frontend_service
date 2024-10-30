5292ea2ae4c5b961a47b07c2739b01c3bf43e827
valens niyonsenga <95166477+valens200@users.noreply.github.com>
2024-10-30 16:36:54 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     apps: [
         {
             name: 'mhab_backend_test',
-            port: '3404', // port in prod 
+            port: '5173', // port in prod 
             exec_mode: 'fork',
             instances: '1',
             // Updated script to run npm run preview
