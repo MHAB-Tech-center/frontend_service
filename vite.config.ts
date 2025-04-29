15dd3909633cad5c528fac80e9d1e8b58ad4cc32
valens200 <valensniyonsenga2003@gmail.com>
2025-04-29 16:27:34 +0200

    refact: upgraded pm2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +10,8 @@ export default defineConfig({
   },
   preview: {
     port: 5173,
-    host: true,
+    // host: true,
+    allowedHosts: true,
   },
   resolve: {
     alias: {
